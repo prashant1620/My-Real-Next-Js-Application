6fa44e26dad2abafd8fed2692704155c59959dfc
Prasanth P <41290306+prashant1620@users.noreply.github.com>
2021-12-06 16:13:06 +0530

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': '719b48b003mshb2bc2715d7ffc14p140d8djsn0aacd756d754'
+      'x-rapidapi-key': ''
     }
   });
     
